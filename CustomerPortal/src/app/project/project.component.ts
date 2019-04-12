f56b51fa8b140734d1b74c8803ff149f59a0d714
Thomas Withaar <thomas.withaar@ovsoftware.com>
2019-04-12 21:59:50 +0200

    project component, project service, slight styling on the wallet

--- CustomerPortal/src/app/project/project.component.ts
+++ CustomerPortal/src/app/project/project.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Project } from './project';
 
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
   styleUrls: ['./project.component.scss'],
 })
-export class ProjectComponent implements OnInit {
+export class ProjectComponent {
+
+  @Input() project: Project;
 
   constructor() { }
-
-  ngOnInit() {}
 
 }
--- CustomerPortal/src/app/vote/vote.page.ts
+++ CustomerPortal/src/app/vote/vote.page.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProjectService } from '../api/project.service';
+
+import { Observable } from 'rxjs';
+
+import { Project } from '../project/project';
 
 @Component({
   selector: 'app-vote',
   templateUrl: 'vote.page.html',
   styleUrls: ['vote.page.scss']
 })
-export class VotePage {}
+export class VotePage implements OnInit {
+
+  public projects$: Observable<Project[]>;
+
+  constructor(private projectService: ProjectService) {}
+
+  ngOnInit(): void {
+    this.projects$ = this.projectService.getProjects();
+  }
+}
