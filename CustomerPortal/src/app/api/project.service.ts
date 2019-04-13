6eceb7969b7d33c2c7e809410705e990e6db408f
Thomas Withaar <thomas.withaar@ovsoftware.com>
2019-04-13 12:51:42 +0200

    Styling for vote page / projects

--- CustomerPortal/src/app/api/project.service.ts
+++ CustomerPortal/src/app/api/project.service.ts
@@ -16,12 +16,14 @@ export class ProjectService {
   public getProjects(): Observable<Project[]> {
     const projWindmills: Project = {
       name: 'Windmills in Spain',
-      description: 'Invest in green energy provided by the Spanish winds!',
+      category: 'energy',
+      description: 'Invest in green energy provided by the Spanish winds! Invest in green energy provided by the Spanish winds! Invest in green energy provided by the Spanish winds!',
       target: 800000
     };
     const projTollRoad: Project = {
       name: 'Finish the Finnish toll roads',
-      description: 'Invest in toll roads for a better flow to Finland',
+      category: 'infrastructure',
+      description: 'Invest in toll roads for a better flow to Finland. Invest in toll roads for a better flow to Finland. Invest in toll roads for a better flow to Finland.',
       target: 3000000
     };
 
--- CustomerPortal/src/app/project/project.ts
+++ CustomerPortal/src/app/project/project.ts
@@ -1,5 +1,6 @@
 export class Project {
     readonly name: string;
+    readonly category: string;
     readonly description: string;
     readonly target: number;
 }
