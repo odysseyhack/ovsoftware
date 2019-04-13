e07556c07fc5c14949a29c23273890c3856d5a69
dudekosse <dudekosse@msn.com>
2019-04-13 19:02:15 +0200

    Added test for getting active users

--- API/src/test/java/com/ovsoftware/reti/UserTests.java
+++ API/src/test/java/com/ovsoftware/reti/UserTests.java
@@ -33,4 +33,24 @@ public class UserTests {
 		assertEquals(user.getWalletAdress(), created.getWalletAdress());
 		userService.deleteUser(created);
 	}
+	
+	@Test
+	public void testGetActiveUsers() {
+		String username1 = "Test1";
+		String username2 = "Test2";
+		String username3 = "Test3";
+		String walletAdress = "WalletHashCode";
+		User user1 = userService.createUser(username1, walletAdress);
+		User user2 = userService.createUser(username2, walletAdress);
+		User user3 = userService.createUser(username3, walletAdress);
+		User created1 = userService.getUser(user1.getUsername());
+		User created2 = userService.getUser(user2.getUsername());
+		User created3 = userService.getUser(user3.getUsername());
+		assertEquals(created1.isActiveParticipant(), true);
+		assertEquals(created2.isActiveParticipant(), true);
+		assertEquals(created3.isActiveParticipant(), true);
+		userService.deleteUser(created1);
+		userService.deleteUser(created2);
+		userService.deleteUser(created3);
+	}
 }
