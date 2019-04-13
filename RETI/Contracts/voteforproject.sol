add0154d3f8cf65833459a9321427cd4caa60dbb
martinaarnoudseOV <martin.aarnoudse@ovsoftware.com>
2019-04-13 16:07:34 +0200

    Added receiveApproval functionality to RETI and created Smart Contract to vote for project

---
+++ RETI/Contracts/voteforproject.sol
@@ -0,0 +1,46 @@
+pragma solidity ^0.5.0;
+
+import { Reti } from "bzz-raw://439ca3dae26f9fd4459c6c34938b61fecbd160f255fc4f4cbe7c0641960a9174";
+
+contract VoteForProject {
+    // describes a Project
+    struct Project {
+        string name;
+        address wallet; 
+        uint numVotes;
+        uint numVotesNeeded;
+    }
+
+    // These state variables are used keep track of the number of Candidates/Voters 
+    // and used to as a way to index them     
+    uint numVoters;
+    address owner;
+    Project project;
+    address _reti;
+    
+    constructor(address reti, string memory name, address wallet, uint numVotesNeeded) public {
+        _reti = reti;
+        project.name = name;
+        project.wallet = wallet;
+        project.numVotesNeeded = numVotesNeeded;
+        owner = msg.sender;
+    }
+    function receiveApproval(address from, uint256 tokens, address token) public {
+        require(token == _reti);
+        Reti reti = Reti(token);
+        if(project.numVotes < project.numVotesNeeded) {
+            reti.transferFrom(from, address(this), tokens);
+            project.numVotes += tokens; 
+            numVoters++;
+        }
+        /* to be implemented: whe numvotes >= votesneeded transfer to wallet of project*/
+    }
+
+    function getNumOfVoters() public view returns(uint) {
+        return numVoters;
+    }
+    
+    function getProject() public view returns (string memory, address, uint, uint) {
+        return (project.name, project.wallet, project.numVotes, project.numVotesNeeded);
+    }
+}
