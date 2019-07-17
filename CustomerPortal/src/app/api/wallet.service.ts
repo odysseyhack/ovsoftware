a7363ae7e9737ae0c9265fb4abae1ed0bc854068
Thomas Withaar <thomas.withaar@ovsoftware.com>
2019-07-17 16:56:33 +0200

    The wallet page now uses web3js to read accounts and balance! Changed/fixed injection of the web3 code

--- CustomerPortal/src/app/api/wallet.service.ts
+++ CustomerPortal/src/app/api/wallet.service.ts
@@ -1,64 +1,64 @@
-import { Injectable } from '@angular/core';
+// import { Injectable } from '@angular/core';
 
-import { BehaviorSubject, Observable } from 'rxjs';
+// import { BehaviorSubject, Observable } from 'rxjs';
 
-import { default as Web3 } from 'web3';
-import { default as contract } from 'truffle-contract';
-import RetiArtifacts from '../../../build/contracts/reti.json';
-declare var web3;
+// import { default as Web3 } from 'web3';
+// import { default as contract } from 'truffle-contract';
+// import RetiArtifacts from '../../../build/contracts/reti.json';
+// declare var web3;
 
-@Injectable({
-  providedIn: 'root'
-})
+// @Injectable({
+//   providedIn: 'root'
+// })
 
-export class WalletService {
+// export class WalletService {
 
-  private reti: contract;
+//   private reti: contract;
 
-  private accounts: string[];
-  private myAccount: string;
+//   private accounts: string[];
+//   private myAccount: string;
 
-  private balance$: BehaviorSubject<number> = new BehaviorSubject(0);
+//   private balance$: BehaviorSubject<number> = new BehaviorSubject(0);
 
-  constructor() {
-    this.initializeReti();
-    this.getAccounts();
-  }
+//   constructor() {
+//     this.initializeReti();
+//     this.getAccounts();
+//   }
 
-  public getBalance(): Observable<number> {
-    return this.balance$.asObservable();
-  }
+//   public getBalance(): Observable<number> {
+//     return this.balance$.asObservable();
+//   }
   
-  private initializeReti(): void {
-    this.reti = contract(RetiArtifacts);
-    this.reti.setProvider(web3.currentProvider);
-  }
+//   private initializeReti(): void {
+//     this.reti = contract(RetiArtifacts);
+//     this.reti.setProvider(web3.currentProvider);
+//   }
 
-  private getAccounts(): void {
-    web3.eth.getAccounts((err, accs) => {
-      if (err != null) {
-        console.error('Something went wrong getting the accounts');
-        return;
-      }
-      if (accs.length === 0) {
-        console.warn('Kon geen rekeningen vinden! Controleer connectie naar Ethereum, 2')
-        return;
-      }
+//   private getAccounts(): void {
+//     web3.eth.getAccounts((err, accs) => {
+//       if (err != null) {
+//         console.error('Something went wrong getting the accounts');
+//         return;
+//       }
+//       if (accs.length === 0) {
+//         console.warn('Kon geen rekeningen vinden! Controleer connectie naar Ethereum, 2')
+//         return;
+//       }
       
-      this.accounts = accs;
-      this.myAccount = accs[0];
+//       this.accounts = accs;
+//       this.myAccount = accs[0];
 
-      this.refreshBalance();
-    });
-  }
+//       this.refreshBalance();
+//     });
+//   }
 
-  private refreshBalance(): void {
-    this.reti.deployed().then(instance => {
-      return instance.balanceOf.call(this.myAccount);
-    }).then(balance => {
-      this.balance$.next(balance);
-    }).catch(function (e) {
-      console.error(e);
-    });
-  }
-}
+//   private refreshBalance(): void {
+//     this.reti.deployed().then(instance => {
+//       return instance.balanceOf.call(this.myAccount);
+//     }).then(balance => {
+//       this.balance$.next(balance);
+//     }).catch(function (e) {
+//       console.error(e);
+//     });
+//   }
+// }
--- CustomerPortal/src/app/wallet/wallet.page.ts
+++ CustomerPortal/src/app/wallet/wallet.page.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { WalletService } from '../api/wallet.service';
-import { Observable } from 'rxjs';
+import { Component, OnInit, Inject } from '@angular/core';
+
+import { Observable, of, from } from 'rxjs';
+import Web3 from 'web3';
+
+import { WEB3 } from '../web3/web3';
+import { map, delay } from 'rxjs/operators';
+// import { WalletService } from '../api/wallet.service';
 
 @Component({
   selector: 'app-wallet',
@@ -11,9 +16,25 @@ export class WalletPage implements OnInit {
 
   public balance$: Observable<number>;
   
-  constructor(private walletService:WalletService) {}
+  constructor(@Inject(WEB3) private web3: Web3) {}
 
   ngOnInit(): void {
-    this.balance$ = this.walletService.getBalance();
+    (window as any).thomasW3 = this.web3; //TODO debug stuff, remove me
+
+    // The first time you use this token in your code you should call the enablemethod of window.ethereum
+    // It's a Promise
+    // this.web3.currentProvider.enable().then( ... );
+    // or async method : await enable();
+
+    from(this.web3.eth.getAccounts()).subscribe(accounts => {
+      console.log ('Ik heb nu in de app accounts gelezen!');
+      console.log(accounts);
+
+      this.balance$ = from(this.web3.eth.getBalance(accounts[0])).pipe(
+        map(Number),
+        delay(1500)
+      );
+    });
+
   }
 }
